796a7b8c906af953959d4a194080a69ee0d6a086
Gustavo <gustavosantosteixeira98@gmail.com>
2025-09-17 17:04:36 -0300

    Resolução e lógica em c.

---
+++ 1010/1010.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[]){
+
+	//componentes convertidos
+	float float_argv3 = atof(argv[3]);
+	float float_argv4 = atof(argv[4]);
+	float float_argv5 = atof(argv[5]);
+	float float_argv6 = atof(argv[6]);
+ 	float valor_a_pagar = float_argv3 * float_argv5 + float_argv4 * float_argv6;
+
+        printf("\n\nCódigo peça 1: %s\nCódigo peça 2: %s\nQuantidades de peças 1: %s\nQuantidades de peças 2: %s\nValor da peça 1: R$%s.00\nValor da peça 2: R$%s.00\nValor total R$%.2f\n\n\n",argv[1],argv[2],argv[3],argv[4],argv[5],argv[6], valor_a_pagar);
+	return 0;
+	
+}
--- 1010/1010_beecrowd.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(char *argc[]){
-  return 0;
-}
