8219d01cc30ba7184930636357d76880a298de27
Gustavo Santos Teixeira <gustavosantosteixeira98@gmail.com>
2025-09-19 17:36:25 -0300

    Alteração 1 em 1122.js

---
+++ 1121_robo_colecionador/1121.js
@@ -0,0 +1,7 @@
+//Criação das estrutura para ler entradas no terminal
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
